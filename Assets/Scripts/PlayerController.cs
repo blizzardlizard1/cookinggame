ca72d1ef62a7b207f9fb3a61a9a1e7ef596b857d
blizzardlizard1 <abdim4515@gmail.com>
2024-11-07 19:15:29 -0500

    Added a script for the player movement. Test is in the sample scene and works with input manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    private Vector3 moveDirection;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovementInput();
+        RotateCharacter();
+    }
+
+    void HandleMovementInput()
+    {
+        // Get input from the Input Manager
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Set move direction based on input
+        moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+    }
+
+    void RotateCharacter()
+    {
+        // Rotate only if there is movement input
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Apply movement based on move direction
+        rb.MovePosition(transform.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
